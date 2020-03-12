bde8758b08b3e12d4e0d56101d891f87eac21321
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2020-03-12 01:19:25 -0300

    Update soma_vetores.asm

--- soma_vetores.asm
+++ soma_vetores.asm
@@ -1,12 +1,12 @@
 	.text 		#demonstra que as instruções a seguir estarão descritas em texto
-	.globl main #denomina nome da funçao do programa principal, como int main (void) {} na linguagem C.
+	.globl main 	#denomina nome da funçao do programa principal, como int main (void) {} na linguagem C.
 
-main:	la $t1,v1 #indicando o vetpr 1 para o registrador t1
-	la $t2,v2 #indicando o vetor 2 para o registrador t2
-	la $t3,v3 #indicando o vetor 3 para o registrador t3
+main:	la $t1,v1	 #indicando o vetpr 1 para o registrador t1
+	la $t2,v2	 #indicando o vetor 2 para o registrador t2
+	la $t3,v3 	#indicando o vetor 3 para o registrador t3
 
-	lw $t4,0($t1) #carregando no registrador t4 o primeiro byte do vetor 1
-	lw $t5,0($t2) #carregando no registrador t5 o primeiro byte do vetor 2
+	lw $t4,0($t1) 	#carregando no registrador t4 o primeiro byte do vetor 1
+	lw $t5,0($t2) 	#carregando no registrador t5 o primeiro byte do vetor 2
 	
 	add $t4,$t4,$t5 #adicionando o valor do registrador t4 + o valor do registrador t5 e reescrevando em t4
 	sw $t4,0($t3)   #guardando o valor de t4 no primeiro byte do vetor 3
@@ -36,5 +36,5 @@ main:	la $t1,v1 #indicando o vetpr 1 para o registrador t1
 v1:	.word 0x12 0xff 0x3 0x14 #.word guarda uma lista de 32 bits 
 v2:	.word 0x12 0xff 0x3 0x14 # ""
 v3:	.word 0x0  0x0  0x0 0x0  # ""
-size:	.word 4			# - 
+size:	.word 4
 
