4684ecaf5f80799ba959dab4fe29e9fb5dfa43df
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2020-04-15 10:08:38 -0300

    Tabuada em MIPS
    Exercício de repetição usando BEQ e índice

---
+++ Tabuada.asm
@@ -0,0 +1,43 @@
+	.text
+	.globl main
+	
+main:   addi $s0,$0,1
+	
+loop:	beq $s0, 11, exit
+	la $s1, valor
+	lw $s1,0($s1)
+	
+	la $a0, frase
+	li $v0, 4
+	syscall
+	
+	la $a0, ($s0)
+	li $v0, 1
+	syscall
+	
+	la $a0, igual
+	li $v0, 4
+	syscall
+	
+	
+	mul $a0,$s0,$s1
+	li $v0,1
+	syscall
+	
+	la $a0, enter
+	li $v0, 4
+	syscall
+
+	addi $s0,$s0,1
+	j loop
+
+
+exit:	li $v0, 10
+	syscall
+
+
+	.data
+valor:	.word 3                    #aqui vai o valor que você deseja que seja a tabuada
+igual:	.asciiz " = "
+frase:	.asciiz "2 x "
+enter:	.asciiz "\n"
